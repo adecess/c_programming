3a8648e1265b9ef7764a4a0bec588676a81af5f2
adecess <axel.saliniere@protonmail.com>
2025-03-02 00:37:41 +0100

    file input

---
+++ file_input_output/input.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		printf("Usage: %s <filename>\n", argv[0]);
+		return 0;
+	}
+
+	int fd = open(argv[1], O_RDWR | O_CREAT, 0644);
+	if (fd == -1) {
+	    perror("open");
+	    return -1;
+	}
+    
+	return 0;
+}
